92d4957a7fa8ddaa73e82c5f57322ff44e855ca8
mellad-morshed <m.morshed@auditstation.me>
2025-07-29 17:28:34 +0400

    add the top section

--- src/app/[locale]/news/layout.tsx
+++ src/app/[locale]/news/layout.tsx
@@ -1,13 +1,20 @@
 import CustomSection from "@/components/ui/CustomSection/CustomSection";
 import FilterLayout from "@/components/ui/FilterDropDown/FilterLayout";
+import TopPagesSection from "@/components/ui/TopPagesSection";
+import { getTranslations } from "next-intl/server";
 
 export default async function newsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const t = await getTranslations();
+
   return (
     <div>
+      <TopPagesSection
+        title={`${t("sections.news.first")} ${t("sections.news.second")}`}
+      />
       <CustomSection className="py-[86px]">
         <div className="flex flex-col lg:flex-row gap-6">
           <div className="w-full lg:w-[274px] shrink-0">
--- src/app/[locale]/our-happy-customers/layout.tsx
+++ src/app/[locale]/our-happy-customers/layout.tsx
@@ -1,13 +1,22 @@
 import CustomSection from "@/components/ui/CustomSection/CustomSection";
 import FilterLayout from "@/components/ui/FilterDropDown/FilterLayout";
+import TopPagesSection from "@/components/ui/TopPagesSection";
+import { getTranslations } from "next-intl/server";
 
 export default async function happyCustomersLayot({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const t = await getTranslations();
   return (
     <div>
+      <TopPagesSection
+        title={`${t("sections.ourhappycustomers.first")} ${t(
+          "sections.ourhappycustomers.second"
+        )}`}
+      />
+
       <CustomSection className="py-[86px]">
         <div className="flex flex-col lg:flex-row gap-6">
           <div className="w-full lg:w-[274px] shrink-0">
