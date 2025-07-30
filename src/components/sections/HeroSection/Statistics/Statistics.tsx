18f1a686d8991b3a4ebf8cc708bb68c095ef5acf
mellad-morshed <m.morshed@auditstation.me>
2025-07-30 19:57:02 +0400

    add margin to statistics

--- src/components/sections/HeroSection/Statistics/Statistics.tsx
+++ src/components/sections/HeroSection/Statistics/Statistics.tsx
@@ -55,7 +55,7 @@ const Statistics = async () => {
   ];
 
   return (
-    <div className="xl:h-[172px] lg:h-[172px] md:h-auto sx:h-auto xs:h-auto  w-full px_padding  xl:absolute lg:absolute md:relative sm:relative xs:relative z-10 xl:bottom-[4%] lg:bottom-[4%]">
+    <div className="xl:h-[172px] lg:h-[172px] md:h-auto sx:h-auto xs:h-auto  w-full px_padding  xl:absolute lg:absolute md:relative sm:relative xs:relative z-10 xl:bottom-[4%] lg:bottom-[4%] md:bottom-[-4%] sm:bottom-[-4%] xs:bottom-[-4%] md:mt-3 sm:mt-3 xs:mt-3">
       <div className="bg-white border-[1px] border-secondary rounded-lg shadow-xl dark:bg-dark-background px-[24px] py-[16px] dark:shadow-dark w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 items-center">
         {statisticsData.map((el, index) => (
           <StatisticsCard
